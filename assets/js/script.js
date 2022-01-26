3d41262c56ab83191d8942c974814a020d6dc3c4
Jundi <indana6969@gmail.com>
2022-01-26 13:17:43 +0700

    finish the intro section

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const lightbox = GLightbox({
+    'href': 'https://www.youtube.com/watch?v=a3ICNMQW7Ok',
+    'type': 'video',
+    'source': 'youtube', //vimeo, youtube or local
+    'width': 900,
+    'autoPlayVideos': 'true'
+});
